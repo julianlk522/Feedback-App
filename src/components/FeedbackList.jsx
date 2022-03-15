3c2141ddde9c8a20755c7f437538e3a2b9d888e4
Julian Lindsay-Kaufman <jjjuliannnn456@gmail.com>
2022-03-15 12:17:32 -0400

    react-icons, delete functionality

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Feedbackitem from './Feedbackitem'
 import PropTypes from 'prop-types'
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
     if (!feedback || feedback.length === 0) {
         return <p>No feedback yet</p>
     }
@@ -10,7 +10,10 @@ function FeedbackList({ feedback }) {
     return (
     <div className='feedback-list'>
         {feedback.map((item) => (
-            <Feedbackitem key={item.id} item={item} />
+            <Feedbackitem 
+            key={item.id} 
+            item={item} 
+            handleDelete={handleDelete}/>
         ))}
     </div>
   )
--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Card from './shared/Card'
 import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa'
 
-function Feedbackitem({item}) {
+function Feedbackitem({item, handleDelete}) {
   
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)}className="close">
+        <FaTimes color='purple'/>
+      </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   )
